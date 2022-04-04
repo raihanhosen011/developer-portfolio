55efbe0ec6cd15502da67150cd5b195f82d28fa2
raihanhosen011 <raihanhosen011@gmail.com>
2022-04-05 01:43:47 +0600

    issue fixed

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,15 +1,9 @@
-import { useSelector } from 'react-redux'
-
 import Layer from './Layer'
-import HomeMeta from '@components/meta/HomeMeta'
 
 export default function Index({ children }) {
-  const { data } = useSelector(state => state)
-  const { homeMetaData } = data
-
   return (
     <>               
-      <HomeMeta homeMetaData={homeMetaData} />
+      {/* <HomeMeta homeMetaData={homeMetaData} /> */}
         {children}  
       <Layer />
     </>

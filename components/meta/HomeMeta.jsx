432b7bbfb5f5dcf3a2a5d2403af3d9086363aea6
Raihan hosen <83702632+raihanhosen011@users.noreply.github.com>
2022-08-27 15:37:06 +0600

    Update HomeMeta.jsx

--- components/meta/HomeMeta.jsx
+++ components/meta/HomeMeta.jsx
@@ -14,13 +14,13 @@ const HomeMeta = () => {
       <meta property="og:title" content='Raihan Hosen : Portfolio' />
       <meta property="og:description" content='Raihan Hosen is a Full Stack Developer, pasionate about implementing incredible solutions and designs using React, Next.js, Node.js and MongoDB. ' />
       <meta property="og:url" content='https://raihanhosen.com/' />
-      <meta property="og:image" content='' />
+      <meta property="og:image" content='https://raihanhosen.com/_next/static/media/raihan-hosen.4bb3afc6.png' />
       <meta property="og:type" content="website" />
       <meta name="linkedin:card" content="summary" />
       <meta name="linkedin:site" content="@raihanhosen01" />
       <meta name="linkedin:creator" content="@raihanhosen01" />
       <meta name="linkedin:description" content='Raihan Hosen is a Full Stack Developer, pasionate about implementing incredible solutions and designs using React, Next.js, Node.js and MongoDB. ' />
-      <meta name="linkedin:image" content='' />
+      <meta name="linkedin:image" content='https://raihanhosen.com/_next/static/media/raihan-hosen.4bb3afc6.png' />
     </Head>
   );
 };

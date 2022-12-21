87aaba35d0061e679c68ef7343c056f4e853ebf8
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-21 16:22:49 +0600

    create Config for social

---
+++ src/common/widgets/Socials/config/constans.tsx
@@ -0,0 +1,12 @@
+import { FiGithub, FiLinkedin, FiTwitter, FiInstagram, FiFacebook } from 'react-icons/fi'
+import { SocialTypes } from './types'
+
+
+// social media data
+export const socialData : SocialTypes[] = [
+    { icon : FiGithub, url : "https://github.com/raihanhosen011" },
+    { icon : FiLinkedin, url : "https://www.linkedin.com/in/raihanhosen01/" },
+    { icon : FiTwitter, url : "https://twitter.com/raihanhosen01" },
+    { icon : FiInstagram, url : "https://instagram.com/raihanhosen0001" },
+    { icon : FiFacebook, url : "https://www.facebook.com/raihanhosen01" },
+]

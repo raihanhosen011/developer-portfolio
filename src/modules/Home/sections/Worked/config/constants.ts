231c34aede0253c5bc93d46518cefadb34335959
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-21 21:48:48 +0600

    add worked_data

---
+++ src/modules/Home/sections/Worked/config/constants.ts
@@ -0,0 +1,44 @@
+import { WorkedTypes } from './types';
+
+// worked data
+export const worked_data : WorkedTypes[] = [
+   { 
+      id: 0,
+      label : "Fiverr",
+      description : [
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+      ],
+      deadline : "2014 - present",
+      title: "As a front end developfder",
+      company : "Fiverr",
+      url : "https://raihanhosen.com",
+   } ,
+   { 
+      id: 1,
+      label : "Upwork",
+      description : [
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+      ],
+      deadline : "2014 - present",
+      title: "As a front end developesdr",
+      company : "Upwork",
+      url : "https://raihanhosen.com",
+   } ,
+   { 
+      id: 2,
+      label : "Dg Team",
+      description : [
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+      ],
+      deadline : "2014 - present",
+      title: "As a front end developerx",
+      company : "Dg Team",
+      url : "https://raihanhosen.com",
+   } ,
+]

669ed34f0b288cf8d9f10d3cf1d3f264dad6837d
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-21 16:27:24 +0600

    Create RightSide component for About

---
+++ src/modules/Home/sections/About/partials/Right.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/legacy/image'
+import React from 'react'
+
+const Right = () => {
+  return (
+    <div className='cols-span-2 lg:block hidden' >
+      <div className='w-[300px] h-[350px] relative group rounded-xl after:transition-all after:duration-200 after:hover:left-[12px] after:hover:top-[12px] after:w-full after:z-30 after:h-full after:border-[2px] after:absolute after:top-[20px] after:left-[20px] after:rounded-xl after:border-sky-500' >
+         <Image 
+            src='/images/model/Raihan Hosen Image, Bangladeshi, The best web developer of bangladesh.jpg'  
+            blurDataURL="/images/model/Raihan Hosen Image, Bangladeshi, The best web developer of bangladesh.jpg"
+            className='rounded-xl z-40 transition-all duration-200 group-hover:translate-y-[2px] group-hover:translate-x-[2px]'
+            layout='fill'
+            objectFit='cover'
+            quality={100}
+            alt='loading...' 
+         />
+         
+         <div className='absolute rounded-xl z-50 top-0 bottom-0 right-0 left-0 bg-sky-700/25 hover:bg-transparent transition-all duration-200 cursor-pointer' />
+      </div> 
+    </div>
+  )
+}
+
+export default Right

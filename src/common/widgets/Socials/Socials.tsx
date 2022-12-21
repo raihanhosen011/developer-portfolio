de5dc812e58d6132a4ee75102914da4253587e21
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-21 16:22:35 +0600

    Create Socials file

---
+++ src/common/widgets/Socials/Socials.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+//
+import { socialData } from './config/constans'
+import { SocialTypes } from './config/types'
+
+const Socials = () => {
+  return (
+    <div id="__social" className='fixed z-[999] bottom-0 left-[40px] hidden xl:block' >
+        <ul className='space-y-6 after:h-[90px] after:w-[1px] after:bg-slate-400 after:block after:mx-auto after:mt-6' > 
+           {socialData.map((e: SocialTypes, i: number) => 
+              <li>
+                <Link 
+                   href={e.url} 
+                   target='_blank'
+                   className='text-[22px] text-slate-400 hover:text-sky-400 cursor-pointer transition-all duration-300 hover:translate-y-[-10px]' 
+                >
+                    {<e.icon />}
+                </Link>
+              </li>
+           )}
+        </ul>  
+    </div>
+  )
+}
+
+export default Socials

399ab4a9eee2770604787591b03e0489581dce6b
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-21 21:49:42 +0600

    Add Page component for motion

---
+++ src/modules/Home/sections/Worked/partials/Pager.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+import { motion } from "framer-motion";
+
+
+function Pager({ children, value } : { children: ReactNode, value: number }) {
+  return (
+    <div className="flex flex-col overflow-hidden w-full" >
+      <motion.div
+        className="flex flex-1 will-change-transform"
+        transition={{
+          tension: 190,
+          friction: 30,
+          mass: 0.5
+        }}
+        initial={false}
+        animate={{ x: value * -100 + "%" }}
+      >
+        {React.Children.map(children, (child, i) => (
+          <div
+            key={i}
+            aria-hidden={value !== i}
+            tabIndex={value === i ? 0 : -1}
+            className='flex flex-col w-full items-stretch justify-start flex-shrink-0 overflow-hidden outline-none'
+          >
+            {child}
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
+}
+
+export default Pager

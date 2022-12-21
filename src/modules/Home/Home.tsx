608e2c0d7ffda8bc2d802efe0edd9cc5da8a0138
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-21 16:23:19 +0600

    Create Home component

---
+++ src/modules/Home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+//
+import About from './sections/About'
+import Hero from './sections/Hero'
+
+
+const Home = () => {
+  return (
+    <>
+
+      <Hero />
+      <About />
+
+    </>
+  )
+}
+
+export default Home

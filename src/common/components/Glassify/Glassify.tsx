22717b687dc22089e2d6b4997c2a9776d8a9c196
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-21 16:20:42 +0600

    create Glassify compoennt

---
+++ src/common/components/Glassify/Glassify.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Glassify = () => {
+  return (
+    <>
+       <img alt="" src="/images/glassify/blur-cyan-0.png" width="530" height="530" decoding="async" data-nimg="1" className="fixed right-full bottom-0 -mr-72 -mb-56 opacity-50" />
+    </>
+  )
+}
+
+export default Glassify

03d5427593f09f1f72821e539c7cba5b5791258d
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-21 21:49:05 +0600

    add types for worked-data

---
+++ src/modules/Home/sections/Worked/config/types.ts
@@ -0,0 +1,10 @@
+// worked data types
+export type WorkedTypes = {
+    label : string,
+    description : Array<string>,
+    deadline : string,
+    title : string,
+    company : string,
+    url : string,
+    id : number
+}

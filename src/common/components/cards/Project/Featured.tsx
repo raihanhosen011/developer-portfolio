93befb43507243c0821e62d1ad0e1ec61488655f
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-22 14:31:33 +0600

    Create Featured Card

---
+++ src/common/components/cards/Project/Featured.tsx
@@ -0,0 +1,89 @@
+import Image from 'next/legacy/image'
+import Link from 'next/link'
+import React from 'react'
+//
+import { HiOutlineExternalLink } from 'react-icons/hi'
+import { FiGithub } from 'react-icons/fi'
+//
+import { cx } from '@config/constants'
+
+
+const Featured = ({ secondary } : { secondary ?: boolean }) => {
+  
+  const __renderImage = () => {
+     return (
+        <div className='col-span-7 relative hidden sm:block' >
+            <Link href='/' >
+                <div className='h-[350px] w-full relative rounded-xl overflow-hidden'>
+                    <Image 
+                        src="/images/projects/project-1.jpg" 
+                        blurDataURL="/images/projects/project-1.jpg"
+                        className='transition-all duration-300 group-hover:scale-110 group-hover:rotate-3'
+                        layout='fill'
+                        objectFit='cover'
+                        quality={100}
+                        alt='loading...' 
+                    />
+
+                    <div className='absolute top-0 bottom-0 left-0 right-0 bg-sky-900/50 transition-all duration-300 cursor-pointer hover:opacity-0' />           
+                </div>  
+            </Link>
+        </div>
+     )
+  }
+
+
+  const __renderContent = () => {
+    return (
+        <div className={cx('col-span-12 sm:col-span-5 text-right sm:absolute sm:left-[50%]', secondary && '!relative z-[100] !left-0 !text-start sm:w-[120%]')} >
+            <div>
+                <p className='text-sky-400' > Featured </p>
+                <h1 className='text-3xl font-medium text-slate-300' > Halycon Team </h1>
+
+                <div className='my-6 p-6 bg-slate-800 text-slate-300 shadow-xl rounded-xl' >
+                    <p>A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.</p>
+                </div>
+
+                <div className={cx('flex text-slate-400 ml-[8rem] gap-x-4 justify-end flex-wrap', secondary && '!justify-start ml-0 mr-[8rem]')} >
+                    <span> react js </span> 
+                    <span> next js </span> 
+                    <span> framer motion </span> 
+                </div>
+
+                <div className={cx('flex justify-end mt-5 gap-3 text-slate-300', secondary && '!justify-start')} >
+
+                    <Link href='/' target='_blank' className='hover:text-sky-400 transition-all duration-300 hover:scale-110 text-2xl' >
+                        <HiOutlineExternalLink />     
+                    </Link>
+
+                    <Link href='/' target='_blank' className='hover:text-sky-400 transition-all duration-300 hover:scale-90 text-[1.2rem]' > 
+                        <FiGithub />     
+                    </Link>
+
+                </div>
+            </div> 
+        </div>
+    )
+  }
+
+
+  return (
+    <div className='relative grid grid-cols-12 items-center gap-3 group' >
+       {secondary ? 
+          <>
+            {__renderContent()}
+            {__renderImage()}
+          </>  
+
+          :
+
+          <>
+            {__renderImage()}
+            {__renderContent()}
+          </>  
+       }
+    </div>
+  )
+}
+
+export default Featured

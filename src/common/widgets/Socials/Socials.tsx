69a490f23f8020cf816a19255522f7ff79ec3047
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-24 09:34:10 +0600

    Add social compoennt

--- src/common/widgets/Socials/Socials.tsx
+++ src/common/widgets/Socials/Socials.tsx
@@ -1,12 +1,16 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
+import { motion } from 'framer-motion'
 //
 import { socialData } from './config/constans'
 import { SocialTypes } from './config/types'
+import { fadeTop, motionStep } from '@config/motion'
 
 const Socials = () => {
   return (
-    <div id="__social" className='fixed z-[999] bottom-0 left-[40px] hidden xl:block' >
+    <motion.div variants={fadeTop} {...motionStep} id="__social" className='fixed z-[999] bottom-0 left-[40px] hidden xl:block' >
         <ul className='space-y-6 after:h-[90px] after:w-[1px] after:bg-slate-400 after:block after:mx-auto after:mt-6' > 
            {socialData.map((e: SocialTypes, i: number) => 
               <li>
@@ -20,7 +24,7 @@ const Socials = () => {
               </li>
            )}
         </ul>  
-    </div>
+    </motion.div>
   )
 }
 

5427e703c020ff852cfc6bc61734466db4ad764f
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-24 09:35:18 +0600

    Add motion for About left

--- src/modules/Home/sections/About/partials/Left.tsx
+++ src/modules/Home/sections/About/partials/Left.tsx
@@ -3,18 +3,18 @@ import { motion } from 'framer-motion'
 import { IoMdArrowDropright } from 'react-icons/io'
 //
 import { skills } from '@modules/Home/config/constanst'
-import { fadeLeft, motionStep } from '@config/motion'
+import { fadeTop, motionStep } from '@config/motion'
 
 
 const Left = () => {
   return (
-    <motion.div variants={fadeLeft} {...motionStep} className='col-span-3' >
+    <motion.div variants={fadeTop} {...motionStep} className='col-span-3' >
 
       <div className='space-y-4 mt-7 text-slate-400' >
 
-        <p>Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!</p>
+        <p>Hello! My name is Raihan and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!</p>
         <p>Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.</p>
-        <p>I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.</p>
+        
         <p>Here are a few technologies I’ve been working with recently:</p>
 
         <div className='__skills md:pr-56' >

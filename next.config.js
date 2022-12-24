4fe2deff6c2669f0da7f098eda58b846463afd10
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-24 12:01:38 +0600

    Edit config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  experimental:{
+    appDir: true
+  }
 }
 
 module.exports = nextConfig

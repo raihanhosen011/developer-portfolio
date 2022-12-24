3e00bb36c2f63074515589aba21dab87627d754b
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-24 09:32:21 +0600

    add motion In page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 "use client"
 
 import React from 'react'
-import { motion } from 'framer-motion'
 //
 import Home from '@modules/Home'
 
 const page = () => {
   return (
-    <motion.div initial='initial' animate='animate' id="__home" >
+    <div id="__home" >
       <Home />
-    </motion.div>
+    </div>
   )
 }
 

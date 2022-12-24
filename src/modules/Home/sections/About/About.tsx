89b767e440104b60db36d0252a0df3dd253887d0
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-24 09:35:04 +0600

    Add motion for About page

--- src/modules/Home/sections/About/About.tsx
+++ src/modules/Home/sections/About/About.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 //
 import Left from './partials/Left'
 import Right from './partials/Right'
+//
+import { fadeLeft, motionStep } from '@config/motion'
+
 
 const About = () => {
   return (
-    <section>
+    <section id='about' >
       
-      <h1 className='flex items-center gap-2 text-3xl font-medium text-slate-300' > 
+      <motion.h1 variants={fadeLeft} {...motionStep} className='flex items-center gap-2 text-3xl font-medium text-slate-300' > 
         <span className='text-sky-400 font-mono' >01. </span>
         About me 
-      </h1>
+      </motion.h1>
 
       <div className='grid grid-cols-1 lg:grid-cols-5 gap-x-20' >
         <Left />

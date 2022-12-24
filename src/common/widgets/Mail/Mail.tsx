60caf6babba779c0593f03cd4b0335647557a661
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-24 09:33:57 +0600

    Add Mail component

--- src/common/widgets/Mail/Mail.tsx
+++ src/common/widgets/Mail/Mail.tsx
@@ -1,13 +1,18 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
+import { motion } from 'framer-motion'
+//
+import { fadeTop, motionStep } from '@config/motion'
 
 const Mail = () => {
   return (
-    <div id="__mail" className='fixed z-[999] bottom-0 right-[40px] hidden xl:block' >
+    <motion.div variants={fadeTop} {...motionStep} id="__mail" className='fixed z-[999] bottom-0 right-[40px] hidden xl:block' >
         <div className='space-y-6 after:h-[90px] after:w-[1px] after:bg-slate-400 after:block after:mx-auto after:mt-6' >
            <Link href='mailto:raihanhosen011@gmail.com' className='text-[15px] text-slate-400 hover:text-sky-400 cursor-pointer transition-all duration-300 hover:translate-y-[-10px] rl' >raihanhosen011@gmail.com</Link> 
         </div>
-    </div>
+    </motion.div>
   )
 }
 

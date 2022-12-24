d7d61750ffc7389c85ec7d724c71c81a1db5526d
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-24 10:00:01 +0600

    Add dynamic Project

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -10,18 +10,16 @@ export const navData = [ "About", "Experience", "Works", "Contact" ]
 // project data
 export const projects : ProjectTypes[] = [
     {
-        id: 0,
         title : "Portfolio website",
         description : "Personal Website V2 Second iteration of my personal website. Designed and developed with a conscious effort to avoid Personal Website V1 Second iteration of my personal website. and Also I add Blog section and saleing system",
-        tech : [ "Next js", "Tailwind CSS", "Express js", "MongoDB" ],
-        code : '',
-        live : '',
-        thumnail : "portfolio-v2",
+        tech : [ "Next js 13", "Tailwind CSS", "Express js", "MongoDB" ],
+        code : 'https://github.com/raihanhosen011/developer-portfolio',
+        live : 'https://raihanhosen.com',
+        thumnail : "portfolio-v2.png",
         featured : true
     },
 
     {
-        id: 0,
         title : "DG Team",
         description : "'Dg team' an company management system. I use this system for front end “Next js, Tailwind CSS, Axios, Redux, Redux-thunk, Formik, Yup, react-query” and for Backend “Node js, Express js, MongoDB, Socket.io, webRtc”",
         tech : [ "Next js", "tailwind CSS", "Redux", "Node js" ],
@@ -31,7 +29,6 @@ export const projects : ProjectTypes[] = [
     },
 
     {
-        id: 0,
         title : "Airebix",
         description : "Airebix is an web development agency website. I use this system for front end “Next js, Tailwind CSS, Axios, Formik, Yup, react-query” and for Backend “Node js, Express js, MongoDB”",
         tech : [ "Next js", "tailwind CSS", "Redux", "Node js" ],
@@ -40,72 +37,19 @@ export const projects : ProjectTypes[] = [
     },
 
     {
-        id: 0,
         title : "Amazon clone",
         description : "I clone World Largest E-commerce website Amazon using react js",
         tech : [ "React js", "Redux" ],
-        code : '',
-        live : '',
-        thumnail : ""
+        code : 'https://github.com/raihanhosen011/amazon-nw',
+        live : 'https://amazon-nw.vercel.app/',
+        thumnail : "amazon.png"
     },
 
     {
-        id: 0,
-        title : "DG Team",
-        description : "",
-        tech : [],
-        code : '',
-        live : '',
-        thumnail : ""
-    },
-
-    {
-        id: 0,
-        title : "DG Team",
-        description : "",
-        tech : [],
-        code : '',
-        live : '',
-        thumnail : ""
-    },
-
-    {
-        id: 0,
-        title : "DG Team",
-        description : "",
-        tech : [],
-        code : '',
-        live : '',
-        thumnail : ""
-    },
-
-    {
-        id: 0,
-        title : "DG Team",
-        description : "",
-        tech : [],
-        code : '',
-        live : '',
-        thumnail : ""
-    },
-
-    {
-        id: 0,
-        title : "DG Team",
-        description : "",
-        tech : [],
-        code : '',
-        live : '',
-        thumnail : ""
-    },
-
-    {
-        id: 0,
-        title : "DG Team",
-        description : "",
-        tech : [],
-        code : '',
-        live : '',
-        thumnail : ""
-    },
+        title : "Powermall",
+        description : "This is an largest e-commerce website of bangladesh",
+        tech : [ "next js", "tailwind css", "MERN" ],
+        live : 'https://powermall.com',
+        thumnail : "powermall.png"
+    }
 ]

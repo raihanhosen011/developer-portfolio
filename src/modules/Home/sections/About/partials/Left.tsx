9b82d94c66b73a8b611ff4bea3bc2a2048fb3f49
Raihan hosen <83702632+raihanhosen011@users.noreply.github.com>
2023-01-07 22:41:49 +0600

    Update Left.tsx

--- src/modules/Home/sections/About/partials/Left.tsx
+++ src/modules/Home/sections/About/partials/Left.tsx
@@ -12,7 +12,7 @@ const Left = () => {
 
       <div className='space-y-4 mt-7 text-slate-400' >
 
-        <p>Hello! My name is Raihan and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!</p>
+        <p>Hello! My name is Raihan and I enjoy creating things that live on the internet. My interest in web development started back in 2020 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!</p>
         <p>Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.</p>
 
         <p>Here are a few technologies I’ve been working with recently:</p>

fcd7a7c3fcb429f16fdf7c9a1eabf6f1d5552aa2
raihanhosen011 <raihanhosen011@gmail.com>
2023-06-06 16:28:42 +0600

    Remove conent

--- src/modules/Home/sections/About/partials/Left.tsx
+++ src/modules/Home/sections/About/partials/Left.tsx
@@ -11,7 +11,7 @@ const Left = () => {
   // content
   const content = [
     "Hello! I'm Raihan, a passionate web developer who loves creating captivating experiences on the internet. My journey into web development began in 2020 when I started experimenting with customizing Tumblr themes. Through tinkering with HTML and CSS, even something as simple as editing a reblog button, I discovered my fascination for building things online.",
-    "Since then, I've had the incredible opportunity to work in diverse environments, including an advertising agency, a start-up, a large corporation, and a student-led design studio. These experiences have shaped my expertise in crafting accessible and inclusive products and digital experiences. Currently, I'm thrilled to be a part of Upstatement, where I collaborate with a range of clients to bring their visions to life.",
+    // "Since then, I've had the incredible opportunity to work in diverse environments, including an advertising agency, a start-up, a large corporation, and a student-led design studio. These experiences have shaped my expertise in crafting accessible and inclusive products and digital experiences. Currently, I'm thrilled to be a part of Upstatement, where I collaborate with a range of clients to bring their visions to life.",
     "I believe in the power of the web to connect people and create meaningful interactions. With a keen eye for detail and a commitment to user-centric design, I strive to deliver exceptional web solutions that leave a lasting impact.",
     "Let's collaborate and turn your ideas into remarkable digital experiences!",
     "Here are a few technologies I have been working with recently:"
